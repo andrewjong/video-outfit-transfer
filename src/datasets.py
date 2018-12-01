6a5b819b1b677dc4e1f3ca0e24bb4c5a6d8d5f0b
Andrew Jong <andrewjong87@gmail.com>
2018-12-01 11:46:48 -0800

    Switch back to torchvision.ToTensor() for bodyseg, to guarantee between 0 and 1. Also add normalization

--- src/datasets.py
+++ src/datasets.py
@@ -1,6 +1,7 @@
 import os
 import random
 from typing import Set, List, Tuple
+import torchvision
 
 import numpy as np
 import torch
@@ -26,27 +27,20 @@ from torch.utils.data import Dataset
 # ] )
 
 
-
 class WarpDataset(Dataset):
     def __init__(
         self,
-        clothing_seg_dir: str,
         body_seg_dir: str,
+        clothing_seg_dir: str,
         min_offset: int = 50,
         random_seed=None,
         transform=None,
+        body_means=None,
+        body_stds=None,
     ):
         """
         Warp dataset for the warping module of SwapNet. All files in the dataset must be
         from the same subject (and ideally with the same background)
-
-
-        Strategy:
-            Get a target clothing segmentation (.npy). Get the matching body
-            segmentation (.png)
-            Choose a random starting clothing segmentation from a different frame
-            (.npy), and perform data augmention on each channel of that frame
-
 
         :param clothing_seg_dir: path to directory containing clothing segmentation
         .npy files
@@ -58,22 +52,29 @@ class WarpDataset(Dataset):
         :param transform: transform for the random drawn clothing segmentation image.
         Note, the transform must be able to operate on a HxWx19-channel tensor
         """
-        self.clothing_seg_dir = clothing_seg_dir
-        # list of file names, mapping to npy arrays
-        self.clothing_seg_files: List[str] = os.listdir(clothing_seg_dir)
-
         self.body_seg_dir = body_seg_dir
         # A set of file names, mapping to RGB images
         # we choose a set for fast lookups
         self.body_seg_files_set: Set[str] = set(os.listdir(body_seg_dir))
-
         # file extension of the body seg images. probably .png or .jpg
         first_bs = next(iter(self.body_seg_files_set))
         self.body_seg_ext = os.path.splitext(first_bs)[-1]
 
+        self.clothing_seg_dir = clothing_seg_dir
+        # list of file names, mapping to npy arrays
+        self.clothing_seg_files: List[str] = os.listdir(clothing_seg_dir)
+
         self.min_offset = min_offset if len(self.clothing_seg_files) < min_offset else 0
         self.random_seed = random_seed
         self.transform = transform
+
+        # transforms for RGB images
+        body_transforms = [torchvision.transforms.ToTensor()]
+        if body_means and body_stds:
+            body_transforms.append(
+                torchvision.transforms.Normalize(body_means, body_stds)
+            )
+        self.body_transforms = torchvision.transforms.Compose(body_transforms)
 
     def __len__(self):
         """
@@ -132,6 +133,11 @@ class WarpDataset(Dataset):
 
     def __getitem__(self, index) -> Tuple[Tensor, Tensor, Tensor]:
         """
+        Strategy:
+            Get a target clothing segmentation (.npy). Get the matching body
+            segmentation (.png)
+            Choose a random starting clothing segmentation from a different frame
+            (.npy), and perform data augmention on each channel of that frame
 
         :param index:
         :return: body segmentation, input clothing segmentation, target clothing
@@ -159,13 +165,12 @@ class WarpDataset(Dataset):
         # the body segmentation that corresponds to the target
         body_seg_file = self._get_matching_body_seg_file(target_cs_file)
         body_seg_img = Image.open(os.path.join(self.body_seg_dir, body_seg_file))
-        body_s = np.array(body_seg_img)
-        body_s = np.moveaxis(body_s, -1, 0)
 
         # convert to PT tensors and return
-        body_s = torch.from_numpy(body_s)
-        input_cs = torch.from__numpy(input_cs_ndarray)
-        target_cs = torch.from_numpy(target_cs_ndarray)
+        # TODO: normalize the tensors
+        body_s = self.body_transforms(body_seg_img)
+        input_cs = torch.FloatTensor(input_cs_ndarray)
+        target_cs = torch.FloatTensor(target_cs_ndarray)
         return body_s, input_cs, target_cs
 
 
