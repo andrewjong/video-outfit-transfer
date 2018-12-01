bd4f9dda877921f1e7f83936422c52c81c5574a1
Andrew Jong <andrewjong87@gmail.com>
2018-11-30 17:24:35 -0800

    Add custom PerPixelCrossEntropyLoss function
    makes the clothing segmentation maps loss function work

---
+++ src/loss.py
@@ -0,0 +1,34 @@
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn.modules.loss import _WeightedLoss
+
+
+class PerPixelCrossEntropyLoss(_WeightedLoss):
+    def __init__(
+        self,
+        weight=None,
+        size_average=None,
+        ignore_index=-100,
+        reduce=None,
+        reduction="elementwise_mean",
+    ):
+        super(PerPixelCrossEntropyLoss, self).__init__(
+            weight, size_average, reduce, reduction
+        )
+        self.ignore_index = ignore_index
+
+    def forward(self, input: Tensor, target: Tensor):
+        b, c, h, w, = input.shape
+        # transform our tensors into a shape that that cross_entropy understands
+        input_unraveled = input.view((b, c, h * w))
+        target_unraveled = target.view((b, c, h*w))
+        # argmax of the channels is the correct label
+        target_labels = target_unraveled.argmax(1)
+
+        return F.cross_entropy(
+            input_unraveled,
+            target_labels,
+            weight=self.weight,
+            ignore_index=self.ignore_index,
+            reduction=self.reduction,
+        )
