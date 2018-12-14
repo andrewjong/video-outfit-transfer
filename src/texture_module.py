252ec99a3247d86927df84a8a16112dcb4eee9c5
Andrew Jong <andrewjong87@gmail.com>
2018-12-13 21:52:04 -0800

    add texture beginning code

---
+++ src/texture_module.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+from maskrcnn_benchmark.layers import ROIAlign
+
+from src.nets import UNetDown, UNetUp, ResidualBlock, DualUNetUp
+
+NUM_ROI = 6
+
+
+class TextureModule(nn.Module):
+    def __init__(self, texture_channels=3, dropout=0.5):
+        super(TextureModule, self).__init__()
+        self.roi_align = ROIAlign(
+            output_size=(128, 128), spatial_scale=1, sampling_ratio=1
+        )
+
+        channels = NUM_ROI * texture_channels
+        self.encode = UNetDown(channels, channels)
+
+        # UNET
+        self.down_1 = UNetDown(channels, 64, normalize=False)
+        self.down_2 = UNetDown(64, 128)
+        self.down_3 = UNetDown(128, 256)
+        self.down_4 = UNetDown(256, 512, dropout=dropout)
+        self.down_5 = UNetDown(512, 1024, dropout=dropout)
+        self.down_6 = UNetDown(1024, 1024, normalize=False, dropout=dropout)
+        self.up_1 = UNetUp(1024, 1024, dropout=dropout)
+        self.up_2 = UNetUp(2 * 1024, 512, dropout=dropout)
+        self.up_3 = UNetUp(2 * 512, 256)
+        self.up_4 = UNetUp(2 * 256, 128)
+        self.up_5 = UNetUp(2 * 128, 64)
+
+        self.upsample_and_pad = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(128, texture_channels, 4, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, input_tex, rois, cloth, target_tex):
+        pooled_rois = self.roi_align(input_tex, rois)
+        encoded_tex = self.encode(pooled_rois)
+
+        scale_factor = input_tex.shape[1] / encoded_tex.shape[1]
+        upsampled_tex = nn.functional.interpolate(
+            encoded_tex, scale_factor=scale_factor
+        )
+
+        # concat on the channel dimension
+        tex_with_cloth = torch.cat((upsampled_tex, cloth), 1)
+        d1 = self.down_1(tex_with_cloth)
+        d2 = self.down_2(d1)
+        d3 = self.down_3(d2)
+        d4 = self.down_4(d3)
+        d5 = self.down_5(d4)
+        d6 = self.down_6(d5)
+        u1 = self.up_1(d6, d5)
+        u2 = self.up_2(u1, d4)
+        u3 = self.up_3(u2, d3)
+        u4 = self.up_4(u3, d2)
+        u5 = self.up_5(u4, d1)
+
+        return self.upsample_and_pad(u5)
---
+++ train_texture_stage.py
@@ -0,0 +1,369 @@
+"""
+Code modified from PyTorch-GAN:
+https://github.com/eriklindernoren/PyTorch-GAN/
+"""
+import argparse
+import os
+import json
+from pprint import pprint
+
+import torch
+
+# from torchvision.utils import save_image
+import torchvision
+from torchvision.utils import save_image
+from tqdm import tqdm
+
+import config
+from src.datasets import WarpDataset, TextureDataset
+from src.loss import PerPixelCrossEntropyLoss
+from src.nets import Discriminator, weights_init_normal
+from src.texture_module import TextureModule
+from src.warp_module import WarpModule
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description="Train the warp stage.",
+)
+parser.add_argument(
+    "-b",
+    "--body_dir",
+    default=config.ANDREW_BODY_SEG,
+    help="Path to folder containing body segmentation images (*.png or *.jpg)",
+)
+parser.add_argument(
+    "-c",
+    "--clothing_dir",
+    default=config.ANDREW_CLOTHING_SEG,
+    help="Path to folder containing clothing segmentation images (*.png or *.jpg)",
+)
+parser.add_argument(
+    "-d", "--dataset_name", default="", help="Name the dataset for output path"
+)
+parser.add_argument(
+    "-o",
+    "--out_dir",
+    default=os.path.join("output", "warp_stage"),
+    help="Output folder path",
+)
+parser.add_argument(
+    "--save_dir",
+    default=os.path.join("models", "warp_stage"),
+    help="Where to store saved model weights",
+)
+parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
+parser.add_argument(
+    "--n_epochs", type=int, default=20, help="number of epochs of training"
+)
+parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
+parser.add_argument(
+    "--adversarial_weight",
+    type=float,
+    default=0.2,
+    help="Factor to scale adversarial loss contribution to warp loss total",
+)
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument(
+    "--b1",
+    type=float,
+    default=0.5,
+    help="adam: decay of first order momentum of gradient",
+)
+parser.add_argument(
+    "--b2",
+    type=float,
+    default=0.999,
+    help="adam: decay of first order momentum of gradient",
+)
+parser.add_argument(
+    "--decay_epoch", type=int, default=100, help="epoch from which to start lr decay"
+)
+parser.add_argument(
+    "--dropout",
+    type=float,
+    default=0.5,
+    help="Probability of dropout on latent space layers",
+)
+parser.add_argument("--img_height", type=int, default=512, help="size of image height")
+parser.add_argument("--img_width", type=int, default=512, help="size of image width")
+parser.add_argument(
+    "--texture_channels",
+    type=int,
+    default=3,
+    help="number of channels in the texture images",
+)
+parser.add_argument(
+    "--sample_interval",
+    type=int,
+    default=500,
+    help="batch interval between sampling of images from generators. --val_dir must be set for this argument to work!",
+)
+parser.add_argument(
+    "--val_dir",
+    help="Path to folder for validation. Use with samping with the --sample_interval argument.",
+)
+parser.add_argument(
+    "--checkpoint_interval",
+    type=int,
+    default=1000,
+    help="batch interval between model checkpoints",
+)
+parser.add_argument(
+    "--n_cpu",
+    type=int,
+    default=8,
+    help="number of cpu threads to use during batch generation",
+)
+parser.add_argument("--gpu", default=0, type=int, help="Set which GPU to train on")
+args = parser.parse_args()
+argparse_dict = vars(args)
+pprint(argparse_dict)
+
+#######################
+# GPU setup
+#######################
+cuda = True if torch.cuda.is_available() else False
+if cuda:
+    torch.cuda.set_device(args.gpu)
+
+
+#######################
+# Make output folders
+#######################
+OUT_DIR = (
+    os.path.join(args.out_dir, args.dataset_name) if args.dataset_name else args.out_dir
+)
+os.makedirs(OUT_DIR, exist_ok=True)
+MODEL_DIR = (
+    os.path.join(args.save_dir, args.dataset_name)
+    if args.dataset_name
+    else args.save_dir
+)
+os.makedirs(os.path.join(MODEL_DIR, "logs"), exist_ok=True)
+
+# Save arguments used
+with open(os.path.join(MODEL_DIR, "logs", "args.json"), "w") as f:
+    json.dump(argparse_dict, f, indent=4)
+# Write progress header
+with open(os.path.join(MODEL_DIR, "logs", "train_log.csv"), "w") as f:
+    f.write("step,loss_D,loss_G,loss_pixel,loss_adversarial\n")
+
+
+###############################
+# Model, loss, optimizer setup
+###############################
+
+# Loss functions
+criterion_GAN = torch.nn.BCELoss()
+criterion_pixelwise = PerPixelCrossEntropyLoss()
+
+# Initialize generator and discriminator
+generator = TextureModule(texture_channels=args.texture_channels, dropout=args.dropout)
+# clothing channels + RGB
+discriminator = Discriminator(in_channels=args.texture_channels + 3)
+
+if cuda:
+    generator = generator.cuda()
+    discriminator = discriminator.cuda()
+    criterion_GAN.cuda()
+    criterion_pixelwise.cuda()
+
+# only load weights if retraining
+if args.epoch != 0:
+    pass
+    # # Load pretrained models
+    # generator.load_state_dict(
+    #     torch.load(
+    #         os.path.join(
+    #             args.save_dir, args.dataset_name, f"generator_{args.epoch}.pth"
+    #         )
+    #     )
+    # )
+    # discriminator.load_state_dict(
+    #     torch.load(
+    #         os.path.join(
+    #             args.save_dir, args.dataset_name, f"discriminator_{args.epoch}.pth"
+    #         )
+    #     )
+    # )
+else:  # if first epoch,
+    # initialize weights
+    generator.apply(weights_init_normal)
+    discriminator.apply(weights_init_normal)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(
+    generator.parameters(), lr=args.lr, betas=(args.b1, args.b2)
+)
+optimizer_D = torch.optim.Adam(
+    discriminator.parameters(), lr=args.lr, betas=(args.b1, args.b2)
+)
+
+
+###############################
+# Datasets and loaders
+###############################
+input_transform = torchvision.transforms.Compose(
+    (
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomAffine(
+            degrees=20, translate=(0.4, 0.4), scale=(0.75, 1.25), shear=(-10, 10)
+        ),
+    )
+)
+texture_dataset = TextureDataset()
+dataloader = torch.utils.data.DataLoader(
+    texture_dataset, batch_size=args.batch_size, num_workers=args.n_cpu
+)
+
+if args.val_dir:
+    val_dataset = TextureDataset()
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, num_workers=args.n_cpu
+    )
+
+
+def sample_images(epoch, batches_done):
+    """Saves a generated sample from the validation set"""
+    bodys, inputs, targets = next(iter(val_dataloader))
+    if cuda:
+        bodys = bodys.cuda()
+        inputs = inputs.cuda()
+        targets = targets.cuda()
+    fakes = generator(bodys, inputs)
+    img_sample = torch.cat((bodys.data, inputs.data, fakes.data, targets.data), -2)
+    save_image(
+        img_sample,
+        os.path.join(OUT_DIR, f"{epoch:02d}_{batches_done:05d}.png"),
+        nrow=args.batch_size,
+        normalize=True,
+    )
+
+
+###############################
+# Training
+###############################
+
+
+def save_models(epoch, batches_done):
+    # Save model checkpoints
+    torch.save(
+        generator.state_dict(),
+        os.path.join(MODEL_DIR, f"generator_{epoch:02d}_{batches_done:05d}.pth"),
+    )
+    torch.save(
+        discriminator.state_dict(),
+        os.path.join(MODEL_DIR, f"discriminator_{epoch:02d}_{batches_done:05d}.pth"),
+    )
+
+
+# Tensor type
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+for epoch in tqdm(
+        range(args.epoch, args.n_epochs), desc="Completed Epochs", unit="epoch"
+):
+    batch = tqdm(dataloader, unit="batch")
+    for i, (bodys, inputs, targets) in enumerate(batch):
+        if cuda:
+            bodys = bodys.cuda()
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        # Adversarial ground truths
+        valid_labels = torch.ones(targets.shape[0]).type(Tensor)
+        fake_labels = torch.zeros(targets.shape[0]).type(Tensor)
+
+        # ------------------
+        #  Train Generators
+        # ------------------
+
+        optimizer_G.zero_grad()
+
+        # GAN loss
+        gen_fakes = generator(body=bodys, cloth=inputs)
+        pred_fake = discriminator(gen_fakes, bodys)
+        loss_adv = criterion_GAN(pred_fake, valid_labels)
+        # Pixel-wise loss
+        loss_pixel = criterion_pixelwise(gen_fakes, targets)
+
+        # Total loss
+        loss_warp = loss_pixel + args.adversarial_weight * loss_adv
+
+        loss_warp.backward()
+
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Real loss
+        pred_real = discriminator(targets, bodys)
+        loss_real = criterion_GAN(pred_real, valid_labels)
+
+        # Fake loss
+        pred_fake = discriminator(gen_fakes.detach(), bodys)
+        loss_fake = criterion_GAN(pred_fake, fake_labels)
+
+        # Total loss
+        loss_D = 0.5 * (loss_real + loss_fake)
+
+        loss_D.backward()
+        optimizer_D.step()
+
+        # --------------
+        #  Log Progress
+        # --------------
+        # Print log
+        batch.set_description(
+            f"[D loss: {loss_D.item():3f}] [G loss: {loss_warp.item():.3f}, "
+            + f"pixel: {loss_pixel.item():.3f}, adv: {loss_adv.item():.3f}]"
+        )
+
+        # Save log to file
+        batches_done = epoch * len(dataloader) + i
+        with open(os.path.join(MODEL_DIR, "logs", "train_log.csv"), "a+") as f:
+            f.write(
+                ",".join(
+                    (
+                        str(batches_done),
+                        str(loss_D.item()),
+                        str(loss_warp.item()),
+                        str(loss_pixel.item()),
+                        str(loss_adv.item()),
+                    )
+                )
+            )
+            f.write("\n")
+
+        # ------------------------------
+        #  Sample images and save model
+        # ------------------------------
+        # If at sample interval save image
+        if args.val_dir and batches_done % args.sample_interval == 0:
+            sample_images(epoch, batches_done)
+
+        if (
+                args.checkpoint_interval != -1
+                and batches_done % args.checkpoint_interval == 0
+        ):
+            save_models(epoch, batches_done)
+
+        # ------------------------------
+        # End train if starts to destabilize
+        # ------------------------------
+        # numbers determined experimentally
+        if loss_D < 0.05 or loss_warp > 3:
+            print(
+                "Loss_D is less than 0.05 or loss_warp > 3!",
+                "Saving models and ending train to prevent destabilization.",
+            )
+            sample_images(-1, -1)
+            save_models(-1, -1)
+            break
+    else:
+        continue
+    break
