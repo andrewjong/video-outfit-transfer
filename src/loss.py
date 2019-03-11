926c80e06ad00b8e9ffdb9647811f3465305e332
Andrew Jong <andrewjong87@gmail.com>
2019-03-11 16:45:58 -0700

    Attempt f1 feature loss

--- src/loss.py
+++ src/loss.py
@@ -1,3 +1,5 @@
+from abc import ABC
+
 import torch.nn.functional as F
 from torch import Tensor
 import torch.nn as nn
@@ -33,13 +35,34 @@ class PerPixelCrossEntropyLoss(_WeightedLoss):
             ignore_index=self.ignore_index,
         )
 
-class FeatureLoss(nn.Module):
+
+class FeatureLoss(ABC, nn.Module):
     def __init__(self, feature_extractor):
+        self.feature_extractor = feature_extractor
+
+class L1FeatureLoss(FeatureLoss):
+    def __init__(self, feature_extractor):
+        super().__init__(feature_extractor)
+        self.loss_fn = nn.L1Loss()
+
+    def forward(self, generated, actual):
+        generated_feat = self.feature_extractor(generated.detach())
+        actual_feat = self.feature_extractor(actual.detach())
+
+        loss = self.loss_fn(generated_feat, actual_feat)
+        return loss
+
+class MultiLayerFeatureLoss(nn.Module):
+    def __init__(self, feature_extractor, num_layers=3):
         # e.g. VGG
         self.feature_extractor = feature_extractor
+        self.layer_weights = [i + 1 / num_layers for i in range(num_layers)]
 
     def forward(self, generated, actual):
         generated_feat = self.feature_extractor(generated)
         actual_feat = self.feature_extractor(actual)
+        # TODO: stuck on how to implement layer weights
+        # maybe this will help: https://forums.fast.ai/t/pytorch-best-way-to-get-at-intermediate-layers-in-vgg-and-resnet/5707/2
+
         pass
 
